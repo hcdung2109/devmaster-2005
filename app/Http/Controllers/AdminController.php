b33a4a6196a9e0f6b3a7bb26c32ea15f47ee65e8
hcdung2109 <hcdung2109@gmail.com>
2020-07-13 19:35:39 +0700

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Order;
+use App\Product;
+use App\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -9,7 +13,19 @@ class AdminController extends Controller
     // trang dashboard
     public function index()
     {
-        return view('admin.dashboard', []);
+        $numOrder = Order::count(); // đếm số đơn hàng
+        $numArticle = Article::count(); // số bài viết
+        $numProduct = Product::count(); // số sản phẩm
+        $numUser = User::count(); // số người dùng
+
+        $data = [
+            'numOrder' => $numOrder,
+            'numArticle' => $numArticle,
+            'numProduct' => $numProduct,
+            'numUser' => $numUser
+        ];
+
+        return view('admin.dashboard', $data);
     }
 
     public static function abc()
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -26,7 +26,13 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
+            <li>
+                <a href="/admin">
+                    <i class="fa fa-calendar"></i> <span>Bảng điều khiển</span>
+                    <span class="pull-right-container"></span>
+                </a>
+            </li>
+            <li class="">
                 <a href="#">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                     <span class="pull-right-container">
@@ -38,6 +44,7 @@
                     <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
                 </ul>
             </li>
+
             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-files-o"></i>
